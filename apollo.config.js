5542ca2e2c917c575826e41ffee55cd3d4f9109c
Matt Zuzolo <mattzuzolo@outlook.com>
2020-10-16 19:31:08 -0400

    Configured project, including env, gitignore, prettier, apollo, typescript and packages

---
+++ apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  service: {
+    addTypename: true, // TOOD: figure out if this is necessary: https://www.apollographql.com/docs/devtools/apollo-config/
+    name: 'whisky',
+  },
+};
